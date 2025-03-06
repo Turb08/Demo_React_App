2cc769544d4ac25309915ce812f8167c5c3fac98
Sharaen Jeevendran <sharaenj6@gmail.com>
2025-03-06 11:34:54 +0530

    Demo project using Next.js

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const UsersPage = () => {
+    return (
+        <div>
+            <button onClick={() => console.log('Click')}>Add to Cart</button>
+        </div>
+    )
+}
+
+export default UsersPage
---
+++ app/users/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const New = () => {
+    return (
+        <div>NewUserPage</div>
+    )
+}
+
+export default New
